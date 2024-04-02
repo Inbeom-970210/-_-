dae1554fe25b6a3a93b3efb5e9e71782236b7bf5
Inbeom Kim <dlsqja970210@naver.com>
2024-04-03 01:53:18 +0900

    day2: Authentication Provider(인증  제공자) 설명 및 구현

---
+++ springsecuritysection5/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.eazybytes.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()   // 인증 필요
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())   // 인증 불필요
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
