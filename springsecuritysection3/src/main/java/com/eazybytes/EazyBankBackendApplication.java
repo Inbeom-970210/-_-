e7ec7017f7f4ee88cb52d3908f17bb72c85fa066
Inbeom Kim <dlsqja970210@naver.com>
2024-04-02 17:15:35 +0900

    day2: 유저 정의와 관리

--- springsecuritysection3/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecuritysection3/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,9 +2,13 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories("com.eazybytes.repository") 외부에 생성했다면 어노테이션 적용
+//@EntityScan("com.eazybytes.model")
 public class EazyBankBackendApplication {
 
     public static void main(String[] args) {
--- springsecuritysection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritysection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -5,7 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,12 +24,71 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((requests) -> requests
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()   // 인증 필요
-                        .requestMatchers("/notices", "/contact").permitAll())   // 인증 불필요
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())   // 인증 불필요
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
 
     }
+
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        *//*
+     * Approach 1 where we use withDegaultPasswordEncoder() method
+     * while creating the user details
+     * *//*
+
+     *//*UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);*//*
+
+     *//*
+     * Approach 2 where we use NoOpPasswordEncoder Bean
+     * while creating the user details
+     * *//*
+
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+
+    }*/
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    /*
+     * NoOpPasswordEncoder is not recommended for production usage.
+     * Use onlu for non-prod.
+     *
+     * @return PasswordEncoder
+     * */
+
+    /*
+    * 비밀번호를 어떻게 저장하였는지 알려주기 위해 항상 필요함.
+    * */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
